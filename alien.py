722015986954869a3c97c63112c0d3f8654abd4d
Betozaver <sara.rzen@gmail.com>
2025-03-04 16:09:38 +0100

    setting up pygame

--- alien.py
+++ alien.py
@@ -1,4 +1,5 @@
 import pygame
+from UbuntuDrivers.detect import set_nvidia_kms
 from pygame.sprite import Sprite
 from random import randint
 
@@ -7,6 +8,8 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load("alien.bmp")
@@ -18,5 +21,17 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Return True if alien is ar edge of screen."""
+        screen_rect = self.screen.get_rect()
+        return(self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+
+
+
+    def update(self):
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
 
 
--- alien_invasion.py
+++ alien_invasion.py
@@ -30,12 +30,14 @@ class AlienInvasion:
         self._create_fleet()
 
 
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -92,6 +94,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -119,7 +126,21 @@ class AlienInvasion:
         new_alien.rect.x = x_position + random_offset
         new_alien.rect.y = y_position + random_offset
         self.aliens.add(new_alien)
-        print(f"x_position: {x_position}, random_offset: {random_offset}")
+
+
+
+    def _check_fleet_edges(self):
+        """Respond appropriately of any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 
     def _update_screen(self):
